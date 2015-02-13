ed8d350a97c639a174aa02d87a242801999bcadb
alleyazahri <shukumei@outlook.com>
2015-02-12 17:05:27 -0700

    Database!
    Check it out!

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Teacher, Student, Score, Problem
 
-# Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Score)
+admin.site.register(Problem)
---
+++ main/migrations/0001_initial.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Problem',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('problem', models.CharField(max_length=10)),
+                ('answer', models.IntegerField()),
+                ('type', models.CharField(max_length=10)),
+                ('level', models.IntegerField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Score',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('level', models.IntegerField()),
+                ('score', models.IntegerField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Student',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=15)),
+                ('last_name', models.CharField(max_length=20)),
+                ('password', models.CharField(max_length=20)),
+                ('current_level', models.IntegerField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Teacher',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=15)),
+                ('last_name', models.CharField(max_length=20)),
+                ('password', models.CharField(max_length=20)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='student',
+            name='teacher',
+            field=models.ForeignKey(to='main.Teacher'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='score',
+            name='student',
+            field=models.ForeignKey(to='main.Student'),
+            preserve_default=True,
+        ),
+    ]
