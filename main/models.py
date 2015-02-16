92fa4ea7ad8020ea14644b1b33f243a963ce6493
alleyazahri <shukumei@outlook.com>
2015-02-16 16:29:44 -0700

    Revert "Again, committing to revert, please ignore"
    This reverts commit 136593a149cea7f57183e355ff10632d12b029b0.

--- main/models.py
+++ main/models.py
@@ -1,45 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-level_choices = (
-    ('1', '1st level'),
-    ('2', '2nd level'),
-    ('3', '3rd level'),
-    ('4', '4th level'),
-    ('5', '5th level'),
-    ('6', '6th level'),
-    ('7', '7th level')
-)
-
-type_choices = (
-    ('M', 'Mixed'),
-    ('A', 'Addition'),
-    ('S', 'Subtraction'),
-    ('P', 'Placement')
-)
-
 class Teacher(models.Model):
-    teacher_username = models.CharField(max_length = 15)
     first_name = models.CharField(max_length = 15)
     last_name = models.CharField(max_length = 20)
     password = models.CharField(max_length = 20)
 
 class Student(models.Model):
-    student_username = models.CharField(max_length = 15)
     first_name = models.CharField(max_length = 15)
     last_name = models.CharField(max_length = 20)
     password = models.CharField(max_length = 20)
-    current_level = models.CharField(max_length = 1, default = '1', choices = level_choices)
+    current_level = models.IntegerField()
     teacher = models.ForeignKey(Teacher)
 
 class Score(models.Model):
-    level = models.models.CharField(max_length = 1, default = '1', choices = level_choices)
+    level = models.IntegerField()
     student = models.ForeignKey(Student)
     score = models.IntegerField()
 
 class Problem(models.Model):
     problem = models.CharField(max_length = 10)
     answer = models.IntegerField()
-    type = models.CharField(max_length = 1, default = 'M', choices = type_choices)
+    type = models.CharField(max_length = 10)
     level = models.IntegerField()
