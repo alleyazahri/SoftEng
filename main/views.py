93ec9e323a6cf32592e16797f3f93bb9d0f79c87
alleyazahri <shukumei@outlook.com>
2015-02-21 18:00:35 -0700

    Working Again! Huzzah!

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from main.models import *
 
-# Create your views here.
+@login_required
+def home(request):
+    #    teacher_table = Teacher.objects.teachers_for_user(request.user)
+    return render(request, "main/home.html")
