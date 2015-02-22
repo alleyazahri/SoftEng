4e137eab2c61073994408b8c36d861b83f477fd5
alleyazahri <shukumei@outlook.com>
2015-02-22 15:55:20 -0700

    Student Main Page
    Awesomeness

--- student/views.py
+++ student/views.py
@@ -6,7 +6,27 @@ from main.models import *
 # Create your views here.
 @login_required
 def home(request):
-    return render(request,'student/home.html',{'name':request.user.username})
+    user = request.user.username
+    count = Student.objects.count()
+    for i in range(count):
+        thing = Student.objects.get(id=i+1)
+        if user == thing.user.username:
+            gameLevel = thing.current_level
+    if gameLevel == 1:
+        return render(request,'student/home.html',{'name':request.user.username})
+    elif gameLevel == 2:
+        return render(request,'student/homeL2.html',{'name':request.user.username})
+    elif gameLevel == 3:
+        return render(request,'student/homeL3.html',{'name':request.user.username})
+    elif gameLevel == 4:
+        return render(request,'student/homeL4.html',{'name':request.user.username})
+    elif gameLevel == 5:
+        return render(request,'student/homeL5.html',{'name':request.user.username})
+    elif gameLevel == 6:
+        return render(request,'student/homeL6.html',{'name':request.user.username})
+    else:
+        return render(request,'student/homel7.html',{'name':request.user.username})
+
 
 @login_required
 def game1(request):
