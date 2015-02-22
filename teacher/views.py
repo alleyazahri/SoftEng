376689968fde4831ab6cabbd093f4caaf5e1af56
alleyazahri <shukumei@outlook.com>
2015-02-22 16:29:53 -0700

    Merge w/Dana p2

--- teacher/views.py
+++ teacher/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from main.models import *
 
 # Create your views here.
 
+@login_required
 def teacherhome(request):
-    #myself = Teacher.objects.filter(teacher_username = )
-    return render(request, "teacher/teacherhome.html", {'teacherName': 'name'})
+    return render(request, "teacher/teacherhome.html", {'username': request.user.username,'firstName': request.user.first_name})
 
+@login_required
 def students(request):
-    #students_table = Student.objects(request.user)
-    #students_for_this_teacher =students_table
-    return render(request, "teacher/students.html")
+    students_table = Student.objects.filter(teacher = request.user)
+    return render(request, "teacher/students.html", {'studentsList': students_table })
 
+@login_required
 def studentinformation(request):
-    return render(request, "teacher/studentdetail")
+    return render(request, "teacher/studentdetail.html")
